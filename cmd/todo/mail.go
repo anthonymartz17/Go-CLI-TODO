f09e54189661a9dd627ab700ac2fe7b75cb81fd2
antonio <antonio.fr.martinezc@hotmail.com>
2025-11-08 15:08:59 -0500

    refatored to dependency injection with constructor for all layers

--- cmd/todo/mail.go
+++ cmd/todo/mail.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/anthonymartz17/Go-CLI-TODO.git/internal/controller"
+	"github.com/anthonymartz17/Go-CLI-TODO.git/internal/db"
 	"github.com/anthonymartz17/Go-CLI-TODO.git/internal/handler"
+	"github.com/anthonymartz17/Go-CLI-TODO.git/internal/repository"
 )
 
 func main(){
-	TodoHandler:=	handler.NewTodoHandler()
+	filePath:="/Users/antoniomartinez/programacion/myProjects/go-projects/Go-CLI-TODO/internal/db/db.json"
+	store:= db.NewStore(filePath)
+repo:= repository.NewRepo(store)
+crtl:= controller.NewController(repo)
+	TodoHandler:=	handler.NewTodoHandler(crtl)
 
 for{
 	fmt.Print("> ")
